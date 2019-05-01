0728be306b8c6043937b8c47ae1f0deed3cbc64e
gitLucasHenrique <lucas.ronqui.souza@gmail.com>
2019-05-01 20:58:40 -0300

    homeless

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = http.createServer(function(req,res){
     if(req.url === '/' || req.url === '/home'|| req.url === '/index'){
         res.writeHead(200, {'Content-Type': 'text/html'});
         fs.createReadStream(__dirname + '/index.html').pipe(res);
-    }if(req.url === '/api/animeList'){
+    }else if(req.url === '/api/animeList'){
         if(req.headers[process.env.HEADER] === process.env.HEADER_VALUE){
             res.writeHead(200, {'Content-Type': 'application/json'});
             res.end(JSON.stringify(jsonFile));
