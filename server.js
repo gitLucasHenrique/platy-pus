85344c0d3055e79f6f9b445df35b98a55d204c3e
gitLucasHenrique <lucas.ronqui.souza@gmail.com>
2019-07-14 18:30:08 -0300

    changed anime list generation

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const server = express();
 require('dotenv').config();
 const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+const URL = 'https://www.animestc.com/'
 
 server.get('/' || '/home' || '/index', (req, res) => {
     res.sendFile(__dirname + '/index.html')
@@ -14,6 +16,24 @@ server.get('/teste', (req, res) => {
     .then(json => res.send(json));
 });
 
+server.get('/getAnime', (req, res) => {
+    fetch(URL)
+    .then(res => res.text())
+    .then((text) => {
+        let strAnimeList = '{\"animes\": ' + JSON.stringify(workData(text)) + '}';
+        fs.writeFile('./assets/json/animeList.json', strAnimeList, function(err){
+            if(err){
+                console.log('failed to write file due: ', err);
+            }
+        });
+        res.send({"output":[{"action":"generate Anime List","status":"success"}]})
+    })
+    .catch(function(error) {
+        log('Request failed', error)
+        res.send({"output":[{"action":"generate Anime List","status":"failed"}]})
+    });
+});
+
 server.get('/api/animeList', (req, res) => {
     res.sendFile(__dirname + '/assets/json/animeList.json');
 });
@@ -23,3 +43,26 @@ server.get('*', (req, res) => {
 });
 
 server.listen(process.env.PORT, () => console.log("listening on port ", process.env.PORT));
+
+function workData(data){
+    let $ = cheerio.load(data);
+    let animeList = [];
+    let animeNames = [];
+    let animeDaysOTW = [];
+    let animeImgs = [];
+    
+    $('.atualizacoes-content').find('.anime-transmissao-nome').each(function(i, value){
+        animeNames.push($('h4', this).text());
+    });
+    $('.atualizacoes-content').find('.transmissao-container').each(function(i, value){
+        animeDaysOTW.push($(value, this).attr('data-semana'));
+    });
+    $('.atualizacoes-content').find('.transmissao-container').find('.anime-transmissao-container').find('img').each(function(i, value){
+        animeImgs.push($(value, this).attr('data-src'));
+    });
+
+    for ( i = 0; i < $('.atualizacoes-content').find('.transmissao-container').length; i++){
+        animeList[i] = { name : animeNames[i], dayOTW : animeDaysOTW[i], img : animeImgs[i] };
+    }
+    return animeList;
+}
