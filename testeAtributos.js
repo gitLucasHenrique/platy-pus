cf5199b53391987685318883b7774915d29375fa
gitLucasHenrique <lucas.ronqui.souza@gmail.com>
2020-01-13 00:20:34 -0300

    test script

--- testeAtributos.js
+++ testeAtributos.js
@@ -4,50 +4,38 @@ const fetch = require('node-fetch');
 function workData(data){
     let $ = cheerio.load(data);
     let animeList = [];
-    let animeNames = [];
-    let animeDaysOTW = [];
-    let animeImgs = [];
-    let animeHoraBR = [];
-    let animeHoraJP = [];
-    let animeReleaseDate = [];
+    let animeURL = [];
 
-    
-    $('.atualizacoes-content').find('.anime-transmissao-nome').each(function(i, value){
-        animeNames.push($('h4', this).text());
-    });
     $('.atualizacoes-content').find('.transmissao-container').each(function(i, value){
-        animeDaysOTW.push($(value, this).attr('data-semana'));
-    });
-    $('.atualizacoes-content').find('.transmissao-container').find('.anime-transmissao-container').find('img').each(function(i, value){
-        animeImgs.push($(value, this).attr('data-src'));
-    });
-    $('.atualizacoes-content').find('.transmissao-container').find('.transmissao-horario').each(function(i, value){
-        animeHoraBR.push($(value, this).attr('data-horariobr'));
-    });
-    $('.atualizacoes-content').find('.transmissao-container').find('.transmissao-horario').each(function(i, value){
-        animeHoraJP.push($(value, this).attr('data-horariojp'));
-        //console.log("valo da iteracao" + i + " " + $(value, this).attr('data-horariojp'))
-    });
-    $('.atualizacoes-content').find('.transmissao-container').find('.transmissao-horario').each(function(i, value){
-        if ($(value, this).attr('data-lancamento') === "0"){
-            console.log(animeReleaseDate)
-            animeReleaseDate.push("?")
-        }else{
-            animeReleaseDate.push($(value, this).attr('data-lancamento'));
-        }
+        animeURL.push($('a', this).attr('href'));
+        //console.log($('a', this).attr('href'))
     });
 
-    //console.log(animeHoraJP);
     for ( i = 0; i < $('.atualizacoes-content').find('.transmissao-container').length; i++){
-        animeList[i] = { name : animeNames[i], dayOTW : animeDaysOTW[i], img : animeImgs[i],
-            transmiBR : animeHoraBR[i], transmiJP : animeHoraJP[i], animeReleaseDate : animeReleaseDate[i] };
+        animeList[i] = { animeURL : animeURL[i] };
     }
     return animeList;
+}
+
+function getAnimeDetails(data){
+    let $ = cheerio.load(data);
+    let anime;
+    let animeDetails = [];
+    anime = $('.pag-anime').find('.pag-anime-dados-direita').find('ul').first().text();
+    animeDetails = anime.toString().trim().replace('\n\n','\n').split('\n');
+    //animeDetails.push(anime)
+    console.log(animeDetails);
 }
 
 fetch("https://www.animestc.com/")
     .then(res => res.text())
     .then((text) => {
-        console.log(JSON.stringify(workData(text), null, 2));
+        //console.log(JSON.stringify(workData(text), null, 2));
         //workData(text);
-    })
+    });
+
+fetch("https://www.animestc.com/animes/boruto-naruto-next-generations-download-legendado-923845/")
+    .then(res => res.text())
+    .then((text) =>{
+        getAnimeDetails(text);
+    });
